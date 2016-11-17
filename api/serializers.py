de6b06c44ce1815a9d180ab742ab0c54d3985be2
Gerben Aaltink <gerbenaaltink@gmail.com>
2016-11-17 07:51:54 +0100

    Minor bug fix; images do not get shown by slack if attachment does not contact text

--- api/serializers.py
+++ api/serializers.py
@@ -10,8 +10,13 @@ def in_channel_response(text=None, sub_text=None, image=None, path=None):
         attachment['text'] = sub_text
 
     if image:
+
         attachment['image_url'] = path + str(image)
         attachment['thumb_url'] = path + str(image)
+
+        if not attachment['text']:
+            # If there is no attachment text, image does not get shown
+            attachment['text'] = ' '
 
     if attachment:
         response['attachments'] = [attachment]
