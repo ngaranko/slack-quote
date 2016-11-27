351847c6832d907e1e67416f55d9e19447a49f55
Dennis Siemensma <github@dennissiemensma.nl>
2016-11-27 21:51:15 +0100

    Switched code coverage testing tools to pytest.

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-python manage.py test --settings=sq.settings_test --parallel
+pytest --ds=sq.settings --cov --cov-report=html:coverage_report/html --cov-report=term
 flake8 *.py */*.py --max-line-length=120
