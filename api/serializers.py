197d23cf25b4d093bbe76cf3433d747fb41d01bd
root <root@wundermonkey.localdomain>
2016-12-08 16:11:47 +0000

    fuck me sideways

--- api/serializers.py
+++ api/serializers.py
@@ -15,20 +15,20 @@ def buttons(buttons):
 def in_channel_response(quote, path=None):
     response = {
         'response_type': 'in_channel',
-        'text': str(quote.gettext()),
+        'text': quote.get_text(),
         'attachments': []
     }
 
     attachment = {}
 
-    if quote.context:
-        attachment['text'] = get_context()
+    if quote.get_context():
+        attachment['text'] = quote.get_context()
 
-    if quote.text_english:
-        attachment['text'] = quote.text_english
+#    if quote.text_english:
+#        attachment['text'] = quote.text_english
 
-    if quote.context_english:
-        attachment['text'] = quote.context_english
+#    if quote.context_english:
+#        attachment['text'] = quote.context_english
 
     if quote.image:
         attachment['image_url'] = path + str(quote.image)
